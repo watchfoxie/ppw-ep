f682918c9753c6d2e8860f6cda12c35cec4429d0
watchfoxie <tocana.mar@gmail.com>
2025-04-14 15:00:57 +0300

    task 8 adjust

--- task8/app.js
+++ task8/app.js
@@ -1,5 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     let progressRunning = false;
+
+    document.body.onload = renderHtmlPage();
+    function renderHtmlPage(){
+        let header = document.createElement('h1');
+        header.textContent = 'To Do: JavaScript tasks 09.apr.2025';
+        document.body.appendChild(header);
+
+        let paragraph = document.createElement('p');
+        paragraph.textContent = 'Tasks category: EP task 8';
+        document.body.appendChild(paragraph);
+
+        let parentDiv = document.createElement('div');
+        parentDiv.id = 'progressWrapper';
+        parentDiv.style.width = '100%';
+        parentDiv.style.backgroundColor = '#CCCCCC';
+        parentDiv.style.height = '30px';
+        document.body.appendChild(parentDiv);
+
+        let childDiv = document.createElement('div');
+        childDiv.id = 'progressBar';
+        childDiv.style.width = '0%';
+        childDiv.style.backgroundColor = '#4CAF50';
+        childDiv.style.height = '100%';
+        parentDiv.appendChild(childDiv);
+        document.body.appendChild(childDiv);
+
+        let htmlButton = document.createElement('button');
+        htmlButton.id = 'startButton';
+        htmlButton.textContent = 'Start Loading';
+        document.body.appendChild(htmlButton);
+
+        let paragraph2 = document.createElement('p');
+        paragraph2.id = 'statusText';
+        paragraph2.textContent = 'Progress: 0%';
+        document.body.appendChild(paragraph2);
+        console.log('executed succesfully!');
+    }
+
     function startLoading(){
         let progress = 0;
         progressRunning = true;
